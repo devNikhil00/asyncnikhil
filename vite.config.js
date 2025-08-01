513594cf41128dbb58f13c7f6bbdfcb6a20201f9
devNikhil00 <nikhilprajapatistaic@gmail.com>
2025-08-01 22:20:40 +0530

    fix: add build configuration to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/asyncnikhil/'
+  base: '/asyncnikhil/',
+    build: {
+    outDir: 'dist', // This is the default output directory
+  },
 })
